4ce647121af09f60b6fbb3c0f4327ebba80d6725
Vulcanric <jneric49@gmail.com>
2023-06-27 02:04:21 -0700

    User-defined Header file used

--- main.h
+++ main.h
@@ -6,12 +6,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/* Structures and Typedef */
+typedef void UNUSED;
 /* Prototypes */
 int _printf(const char *format, ...);
 int print_integers(const char *format, ...);
 int print_dec_bin(const char *format, ...);
 
 /* Other important prototypes */
+int _putchar(char c);
 unsigned int _strlen(char *str);
 char *_strcpy(char *dest, char *src);
 #endif
