481c2cb9647ca94d9cad16b687f2a16c97f9b81a
Vulcanric <jneric49@gmail.com>
2023-06-26 17:45:14 -0700

    Function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -10,44 +10,48 @@
 int _printf(const char *format, ...)
 {
 	int i = 0, counter = 0;
-	char *buff, ch;
+	char *buff;
 	size_t buffsize;
 	va_list var;
 
 	va_start(var, format);
-	while (format[i]) /* Iterating through the characters of format */
+
+	if (format) /* IF format is not NULL */
 	{
-		if (format[i] == '%') /* IF a '%' character is found ...*/
+		while (format[i]) /* Iterating through the characters of format */
 		{
-			switch (format[i + 1]) /* Examine the next character*/
+			/* Allocating memory for buffer */
+			buff = malloc(sizeof(char));
+			if (buff == NULL) /* Handling malloc return */
+				return -1;
+
+			*buff = format[i];
+			if (format[i] == '%') /* IF a '%' character is found ...*/
 			{
-				case 'c':
-					ch = va_arg(var, int);
-					write(1, &ch, 1);
-					break;
-				case 's':
-					buff = va_arg(var, char *);
-					buffsize = _strlen(buff);
-					write(1, buff, buffsize);
-					counter += buffsize - 1;
-					break;
-				case '%':
-					ch = '%';
-					write(1, &ch, 1);
-					break;
-				default:
-					break;
+				switch (format[i + 1]) /* Examine the next character*/
+				{
+					case 'c':
+						*buff = va_arg(var, int);
+						break;
+					case 's':
+						_strcpy(buff, va_arg(var, char *));
+						break;
+					case '%':
+						*buff = '%';
+						break;
+					default:
+						break;
+				}
+				i++; /* To jump over the conversion char */
 			}
-			counter++;
+			buffsize = _strlen(buff);
+			write(1, buff, buffsize);
 			i++;
+			counter += buffsize;
+			free(buff);
 		}
-		else
-		{
-			write(1, &format[i], 1);
-			counter++;
-		}
-		i++;
 	}
+	va_end(var);
 	return (counter);
 }
 
@@ -66,4 +70,25 @@ unsigned int _strlen(char *str)
 		len++;
 	}
 	return (len);
+}
+
+/**
+ * _strcpy - Copies the content of a string into another string
+ * @dest: destination string of the copied string
+ * @src: source string of the copied string
+ * Return: destination string having a copy of source string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+		i++;
+
+	while (i >= 0)
+	{
+		dest[i] = src[i];
+		i--;
+	}
+	return (dest);
 }
