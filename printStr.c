780440b02b45eb45e59e35c4d51379f800b1b566
Vulcanric <jneric49@gmail.com>
2023-06-29 15:47:58 -0700

    Function that handles printing of the value of the %s format specifier

---
+++ printStr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * printStr - Prints a string when the conversion specifier given is 's'
+ * @buff: Buffer to hold the converted format specifier
+ * @ap: List type "all" of the variable arguments
+ * @flag: Not handled
+ * @width: Not handled
+ * @precision: Not handled
+ * @len_modif: Not handled
+ * Return: Number of characters printed
+ */
+int printStr(va_list ap, char *buff, int flag, int width,
+		int precision, int len_modif)
+{
+	int char_printed = 0;
+	(void)flag;
+	(void)width;
+	(void)precision;
+	(void)len_modif;
+
+	buff = va_arg(ap, char *);
+	if (buff == NULL)
+	{
+		buff = "(null)";
+		printBuffer(buff);
+	}
+
+	else
+		char_printed = printBuffer(buff);
+
+	return (char_printed);
+}
