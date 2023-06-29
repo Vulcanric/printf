0340399623e3b4971c03ad220448ccc38487d80a
Vulcanric <jneric49@gmail.com>
2023-06-29 15:55:20 -0700

    Function that handles all format specifiers and their functions

---
+++ handleformt.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * printAll - Handles all format specifier conversion
+ * @conv_spec: Conversion specifier character called
+ *
+ * Return: The function to handle the formatted printing
+ */
+int (*handleformt(char conv_spec))(va_list ap, char *buff, int, int, int, int)
+{
+	format functions[] = {
+		{'c', printChar},
+		{'s', printStr},
+		{0, 0}
+	};
+	int i = 0;
+
+	while (functions[i].conv_spec != 0 && functions[i].conv_spec != conv_spec)
+	{
+		i++;
+	}
+	if (functions[i].conv_spec == 0)
+		return (0);
+
+	return (functions[i].func);
+}
