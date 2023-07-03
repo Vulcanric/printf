ad0964e6c5c860ebfa134a55e7b2eecf4468401a
Vulcanric <jneric49@gmail.com>
2023-07-02 19:18:56 -0700

    Function that handles printing string in reverse

--- printRev.c
+++ printRev.c
@@ -3,27 +3,31 @@
 /**
  * printRev - prints a string in reverse
  * @ap: List "all" of variable arguments
- * @buff: handled.
+ * @buff: Temporarily holding conversion for printing
  * @flag: Not handled.
  * @width: Not handled.
  * @precision: Not handled.
  * @len_modif: Not handled.
  * Return: Number of chars printed
  */
-int printRev(va_list ap, char __attribute__((unused))*buff,
-		int flag, int width, int precision, int len_modif)
+int printRev(va_list ap, char *buff, int flag, int width,
+		int precision, int len_modif)
 {
-	int i, l = 0, char_printed = 0;
-	char *str = va_arg(ap, char *);
+	int i, len = 0, char_printed = 0;
+	(void)ap;
+	(void)flag;
+	(void)width;
+	(void)precision;
+	(void)len_modif;
 
-	for (i = 0; str[i]; i++)
-		l++;
-	l--;
+	buff = va_arg(ap, char *);
+	for (i = 0; buff[i]; i++)
+		len++;
 
-	while (l > 0)
+	for (; len >= 0; len--)
 	{
-		char_printed += _putchar(str[i]);
-		l--;
+		char_printed += _putchar(buff[len]);
 	}
+
 	return (char_printed);
 }
