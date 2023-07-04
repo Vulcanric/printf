01be65509dfd817f6d9f6ff87932648b44317bf6
Vulcanric <jneric49@gmail.com>
2023-07-03 17:33:45 -0700

    Function that handles the 'X' specifier, prints Hex in uppercase

--- printHex_upper.c
+++ printHex_upper.c
@@ -1,40 +1,38 @@
 #include "main.h"
 
 /**
- * printHex_upper - Prints unsigned integers in uppercase
+ * printHex_upper - Prints unsigned integer in hexa-decimal, uppercased
  * @ap: List "all" of variable arguments
- * @buff: Buffer to hold conversion for printing. Not handled.
- * @flag: handled.
- * @width: handled.
- * @precision: Not handled.
- * @len_modif: handled.
+ * @buff: Buffer not handled.
+ * @flag: Handled
+ * @width: Handled
+ * @precision: Not handled
+ * @len_modif: Handled
+ * Return: Number of digits printed
  */
 int printHex_upper(va_list ap, char __attribute__((unused))*buff, int flag,
 		int width, int precision, int len_modif)
 {
-	int padd, num_D_printed = 0;
-	unsigned int num = va_arg(ap, unsigned int);
-	unsigned int numRep = num;
+	unsigned int num = va_arg(ap, unsigned int), numRep = num;
+	int num_D_printed = 0, padd;
 	char upperhex[] = "0123456789ABCDEF";
-
 	(void)precision;
 
-	if (flag == '-') /* Left Justification */
+	if (len_modif == 'l')
 	{
-		num_D_printed += print_Hex(upperhex, num);
-		padd = width - num_D_printed;
-		for (; padd > 0; padd--)
-			num_D_printed += _putchar(' ');
-
-		return (num_D_printed);
+		num = (long)num;
+	}
+	else if (len_modif == 'h')
+	{
+		num = (short)num;
 	}
 
-	else if (flag == '0') /* Right Justification */
+	if (flag == '0')
 	{
 		while (numRep > 15)
 		{
 			numRep /= 16;
-			num_D_printed += 1;
+			num_D_printed++;
 		}
 		padd = width - num_D_printed;
 		while (padd > 0)
@@ -42,21 +40,20 @@ int printHex_upper(va_list ap, char __attribute__((unused))*buff, int flag,
 			num_D_printed += _putchar('0');
 			padd--;
 		}
-		print_Hex(upperhex, num);
-
+	}
+	if (flag == '-')
+	{
+		num_D_printed += print_Hex(upperhex, num);
+		padd = width - num_D_printed;
+		while (padd > 0)
+		{
+			num_D_printed += _putchar(' ');
+			padd--;
+		}
 		return (num_D_printed);
 	}
-
-	else if (flag == '#')
+	if (flag == '#')
 		num_D_printed += write(1, "0x", 2);
-
-	/* Handling length modifier */
-	if (len_modif == 'l')
-		num = (long)num;
-
-	else if (len_modif == 'h')
-		num = (short)num;
-
 
 	num_D_printed += print_Hex(upperhex, num);
 
